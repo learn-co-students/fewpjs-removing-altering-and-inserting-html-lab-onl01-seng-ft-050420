052248db7c090ac576b9e2b6e02491d4e67cec26
leobr <leobrionesherrera@gmail.com>
2020-08-05 13:44:42 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+
+let hOne = document.createElement('h1');
+
+hOne.setAttribute('id', 'victory');
+
+document.body.appendChild(hOne);
+
+let newHeader = document.getElementById('victory');
+
+newHeader.innerHTML = "Leonardo is the champion";
+
