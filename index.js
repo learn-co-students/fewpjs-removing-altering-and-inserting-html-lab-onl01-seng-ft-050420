2e8c9a90f82929a7b3f59eaebe42c9a67c170bc2
millardjimmy <millardjimmy@gmail.com>
2020-08-08 17:27:35 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+// Test 1
+let main = document.getElementById('main');
+main.remove();
+
+// Test 2
+let newHeader = document.createElement('h1');
+
+// Test 3
+newHeader.id = "victory"
+
+// Test 4
+newHeader.innerHTML = "YOUR-NAME is the champion"
